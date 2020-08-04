4abe6936113e654a123fc904ec8b38bf96525f85
Josh Pilkington <j.pilkington@hotmail.com>
2020-08-04 16:13:09 +0100

    Added career tab

--- app/core/routes.py
+++ app/core/routes.py
@@ -6,3 +6,8 @@ from app.core import bp
 @bp.route('/')
 def homepage():
     return render_template('core/homepage.html')
+
+
+@bp.route('/career')
+def career_homepage():
+    return render_template('core/career.html')
